40f214e83855f4729298cf106865be3689002a10
bdimir <git config --global user.email {ID}+{username}@users.noreply.github.com`>
2019-07-25 22:57:52 +0300

    adding cors

--- sebspringbootrest/src/main/java/seb/controller/ProductsController.java
+++ sebspringbootrest/src/main/java/seb/controller/ProductsController.java
@@ -3,6 +3,7 @@ package seb.controller;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,7 @@ import seb.repository.rules.RulesRepository;
 @RestController
 public class ProductsController {
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(value = "/products", method = RequestMethod.POST)
 	public ResponseEntity<List<String>> getProducts(@RequestBody PersonAnswers personAnswers) {
 		IProductRulesRepository rules = new RulesRepository();
