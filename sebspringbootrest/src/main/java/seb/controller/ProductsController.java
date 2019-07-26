e3e23b653ee75478e7d900f9098dd3c9a495e39d
bdimir <git config --global user.email {ID}+{username}@users.noreply.github.com`>
2019-07-26 08:57:00 +0300

    fixing import for cors

--- sebspringbootrest/src/main/java/seb/controller/ProductsController.java
+++ sebspringbootrest/src/main/java/seb/controller/ProductsController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import seb.models.dto.PersonAnswers;
 import seb.products.service.IProductsOfferService;
 import seb.products.service.ProductsOfferService;
@@ -19,7 +18,6 @@ import seb.repository.rules.RulesRepository;
 
 @RestController
 public class ProductsController {
-
 	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(value = "/products", method = RequestMethod.POST)
 	public ResponseEntity<List<String>> getProducts(@RequestBody PersonAnswers personAnswers) {
